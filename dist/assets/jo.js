7f54c22a4a48362f70574b16c327d4cb723ea912
jose-27 <bdj19@hotmail.com>
2016-03-26 23:08:07 -0400

    adding the login form

--- dist/assets/jo.js
+++ dist/assets/jo.js
@@ -385,7 +385,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("jo/app")["default"].create({"name":"jo","version":"0.0.0+3b2af17f"});
+  require("jo/app")["default"].create({"name":"jo","version":"0.0.0+692983a7"});
 }
 
 /* jshint ignore:end */
